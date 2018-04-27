20abbf22ea43d97a61613b11a741f3d8bb66a117
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-04-27 10:22:09 +0100

    CLing each name from api return

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,9 @@
 console.log("loaded")
 
-fetch('https://swapi.co/api/people/1/')
+//getting error because chrome does not allow localHost to localHost 
+fetch('https://swapi.co/api/people/')
 .then(response => response.json())
-.then(jsonData => console.log(jsonData))
+.then(jsonData => showResults(jsonData))
 .catch(function(error)
 {
     console.log(error)
@@ -10,3 +11,19 @@ fetch('https://swapi.co/api/people/1/')
 
 //look into how to add no-cors in the fetch.
 
+
+function showResults(jsonDataResult){
+    const resultsSection = document.querySelector('#results')
+    resultsSection.innerHTML = '';
+    let html = '';
+    console.log(jsonDataResult.results[0])
+    //let results = jsonDataResult.results
+    //console.log(result)
+    jsonDataResult.results.forEach(person => {
+        //console.log("working?")
+         console.log(person.name)   })   
+    };
+
+    //console.log(jsonDataResult.results)
+    //jsonDataResult.forEach(person => {
+       // console.log(person.name) 
