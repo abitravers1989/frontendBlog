2871fbded8a53f4bd8e533311d9febbf8e84684b
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-05-09 12:24:01 +0100

    Refactoring mustache into css file.

---
+++ game/game.js
@@ -0,0 +1,64 @@
+:root {
+
+    --lightgrey: #F7F7F7;
+    --darkblue: #00151C;
+    --blue: #002C4C;
+    --midblue: #17A6D6;
+    --lighblue: #17A6D6;
+
+    --white: #fff;
+    --blue: #1f2a5b;
+    --grey:  #f7f7f8;
+
+}
+
+body {
+    background: var(--blue);  
+}
+
+.mustache{
+    
+    width: 180px;
+    height: 180px;
+    left: 100px;
+    border-radius: 50%;
+    position: absolute;
+    margin: 10%;
+
+    color: var(--white);
+
+    box-shadow: 
+    150px 240px 0 0 currentColor,
+    300px 240px 0 0 currentColor;
+}
+
+.mustache::before {
+    content: "";
+    position: absolute;
+    left: 30px;
+    top: 120px;
+
+    width: 210px;
+    height: 120px;
+    border-bottom: solid 180px currentColor;   
+    border-radius: 0 0 0 100%;
+
+    transform: rotate(-40deg);
+    transform-origin: right 210px;
+}
+
+
+.mustache::after{
+    content: "";
+    position: absolute;
+    left: 390px;
+    top: 120px;
+
+    width: 210px;
+    height: 120px;
+    border-bottom: solid 180px currentColor;   
+    border-radius: 0 0 100% 0;
+
+    transform: rotate(40deg);
+    transform-origin: left 210px;
+}
