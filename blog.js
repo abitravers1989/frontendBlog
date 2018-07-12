5b6976635524a6501e8d2137235ecf6a8718aff2
abitravers1989 <atravers4007@gmail.com>
2018-07-12 21:23:24 +0100

    Adding create blog post button and area

--- blog.js
+++ blog.js
@@ -1,5 +1,6 @@
 function init(){
     addEventListener2()
+    clickedCreatePost()
 }  
 
 fetch('http://localhost:5000/posts')
@@ -33,5 +34,17 @@ function goToNewPage(e){
     const blogTitle = event.target.attributes[0].value
     window.location.href = `template.html/?${blogTitle}`
 }
+
+function clickedCreatePost(){
+    const createPostDiv = document.querySelector('.createBlog');
+    createPostDiv.addEventListener("click", createTextBox)
+}
+
+function createTextBox(){
+    const textArea = document.querySelector('.textArea');
+    console.log(textArea);
+    textArea.style.visibility = 'visible'; 
+}
+    
 
 init()
