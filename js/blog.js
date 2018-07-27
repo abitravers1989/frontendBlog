ae68482cf1513b4780aef84b1c9eac62935ed75d
abitravers1989 <atravers4007@gmail.com>
2018-07-27 18:02:11 +0100

    fix mobile layout but not desktop

--- js/blog.js
+++ js/blog.js
@@ -1,57 +1,56 @@
 
-function init(){
+function init() {
     addEventListener2()
     clickedCreatePost()
-}  
+}
 
 fetch('https://api.abitravers.com/posts')
     .then(response => response.json())
     .then(jsonResponse => listBlogs(jsonResponse))
-.catch(function(error)
-{
-    console.log(error)
-});
+    .catch(function (error) {
+        console.log(error)
+    });
 
-function listBlogs(jsonData){
+function listBlogs(jsonData) {
     const resultsSection = document.querySelector('.showResults')
     resultsSection.innerHTML = '';
     let html = '';
     jsonData.forEach(post => {
         let formattedTime = moment(post.postTime).format('MMMM Do YYYY');
         // let formattedTime = moment(post.postTime).format('MMMM Do YYYY, h:mm:ss a');
-        html+=
-        `<li> 
+        html +=
+            `<li> 
             <div data-href="${post.title}"> ${formattedTime}:        ${post.title}</div> 
         </li>`
-});
+    });
     resultsSection.innerHTML = html;
 }
 
-function clickedSearchButton(){
+function clickedSearchButton() {
 
 }
 
-function searchTitle(){
+function searchTitle() {
 
 }
 
-function addEventListener2(){
+function addEventListener2() {
     const showResults = document.querySelector('.showResults');
     showResults.addEventListener('click', goToNewPage)
 }
 
-function goToNewPage(e){
+function goToNewPage(e) {
     event.stopPropagation();
     const blogTitle = event.target.attributes[0].value
     window.location.href = `template.html/?${blogTitle}`
 }
 
-function clickedCreatePost(){
+function clickedCreatePost() {
     const createPostDiv = document.querySelector('.createBlog');
     createPostDiv.addEventListener('click', createTextBox)
 }
 
-function createTextBox(){
+function createTextBox() {
     const textArea = document.querySelector('.textArea');
     textArea.classList.toggle('is-visible');
 }
@@ -61,28 +60,28 @@ const textArea = document.querySelector('.textArea');
 const saveButton = document.getElementsByTagName("button");
 saveButton[1].addEventListener('click', postTexttoAPI);
 
-function postTexttoAPI(){
-    const headerData = { 'Content-Type':'application/json', 'clinetPassword': 'xxxxx'}
+function postTexttoAPI() {
+    const headerData = { 'Content-Type': 'application/json', 'clinetPassword': 'xxxxx' }
     const text = textArea.value
-    const bodyFromPage = { 'content': text, userName: "Abi", title: "NewTest"}
+    const bodyFromPage = { 'content': text, userName: "Abi", title: "NewTest" }
     try {
         fetch('https://api.abitravers.com/posts', {
             method: 'POST',
             headers: headerData,
             body: JSON.stringify(bodyFromPage)
-            
+
         })
-        //add stream reader for readablestream
-        .then(function(response){ 
-            return console.log("post" + textArea.value + "Response Status" + response.status)
-        })
+            //add stream reader for readablestream
+            .then(function (response) {
+                return console.log("post" + textArea.value + "Response Status" + response.status)
+            })
     }
-       //add logging in
-    catch(error){
+    //add logging in
+    catch (error) {
         console.log(error);
-    }   
+    }
 }
-    
+
 
 init()
 
